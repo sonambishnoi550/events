72ebfdc3d8ca25aabaef7b26bcf2f28b799f4784
Sonam Bishnoi <apple@apples-Laptop.local>
2024-07-25 14:44:38 +0530

    mistake solve

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,7 +1,7 @@
 let nav = document.querySelector("nav");
 
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 350) {
+    if (window.scrollY > 2000) {
         nav.style.backgroundColor = "black";
     }
     else {
